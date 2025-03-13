9a86a0aade517e89267b14f1a912fa61f46854fe
ishan.zerobit <ishan.zerobit@gmail.com>
2025-03-13 14:52:27 +0530

    fixed manifest file issue

--- app/layout.js
+++ app/layout.js
@@ -11,5 +11,13 @@ export default function Layout({ children }) {
     }
   }, []);
 
-  return <>{children}</>;
+  return (
+    <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
